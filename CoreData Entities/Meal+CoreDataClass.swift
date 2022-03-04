19150b7296982288e9904194f72d3cfc0d441f14
Belverk070 <vometlin@gmail.com>
2022-03-04 20:59:56 +0300

    First commit of project with CoreData

---
+++ CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Василий Метлин on 04.03.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Василий Метлин on 04.03.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
